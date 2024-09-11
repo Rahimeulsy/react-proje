459a716122ab1dc852b7022787a02e2b4c24320e
RahimeUlusoy <rulusoy.2516@gmail.com>
2024-09-11 09:26:12 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Contacts from './component/Contacts';
+
+function App() {
+  return (
+    <div className="App">
+      <Contacts/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Contacts/index.js
@@ -0,0 +1,31 @@
+import {useState, useEffect} from 'react';
+import List from './List';
+import Form from './Form';
+
+function Contacts() {
+    const[contacts, setContacts] = useState([
+      {
+        fullname: 'Mehmet',
+        phone_number:'123123'
+      },{
+        fullname:'Ayşe',
+        phone_number:'456456'
+      },{
+        fullname:'Nazlı',
+        phone_number:'789785'
+      }
+    ]);
+
+    useEffect(()=>{
+console.log(contacts);
+    },[contacts]);
+
+  return (
+    <div id="container1">
+        <List contacts={contacts}/>
+        <Form addContact={setContacts} contacts={contacts}/>
+    </div>
+  );
+}
+
+export default Contacts;
